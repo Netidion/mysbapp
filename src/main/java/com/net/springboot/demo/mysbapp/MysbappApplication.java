bc68fbf2fd5fed2840bba18fa0c282ae151e7896
Neti <98841356+Netidion@users.noreply.github.com>
2023-12-03 13:30:17 +0200

    Add files via upload

---
+++ src/main/java/com/net/springboot/demo/mysbapp/MysbappApplication.java
@@ -0,0 +1,13 @@
+package com.net.springboot.demo.mysbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/net/springboot/demo/mysbapp/MysbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.springboot.demo.mysbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
