77ac1984b7fe7ecde60309be0ad4a2006b3f552e
Neti <98841356+Netidion@users.noreply.github.com>
2023-12-04 20:47:22 +0200

    Adding dev-tools for hot reloading

--- src/main/java/com/net/springboot/demo/mysbapp/rest/FunRestController.java
+++ src/main/java/com/net/springboot/demo/mysbapp/rest/FunRestController.java
@@ -10,4 +10,17 @@ public class FunRestController {
     public String sayHello(){
         return "Yo there!!!";
     }
+
+    //expose new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    //expose a new endpoint for fortune
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+
 }
