dfd422e34caf2e6659e0741c91625324bafce29a
Neti <98841356+Netidion@users.noreply.github.com>
2023-12-05 10:56:13 +0200

    Injecting custom application properties

--- src/main/java/com/net/springboot/demo/mysbapp/rest/FunRestController.java
+++ src/main/java/com/net/springboot/demo/mysbapp/rest/FunRestController.java
@@ -1,10 +1,25 @@
 package com.net.springboot.demo.mysbapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for team.leader.name and team.name
+    @Value("${team.leader.name}")
+    private String teamLeaderName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for teamInfo
+    @GetMapping("/team-info")
+    public String getTeamInfo() {
+        return "Team Leader: " + teamLeaderName + ", Team Name: " + teamName;
+    }
+
     //expose "/" for saying hello
     @GetMapping("/")
     public String sayHello(){
